b45b8dbb105c58b5202750bb90a08e8a1aa38deb
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2023-01-15 10:03:10 -0800

    js array practice

---
+++ array_practice.js
@@ -0,0 +1,5 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let firstEven = numbers.find((num) => num % 2 === 0);
+console.log(firstEven); // Output: 2
+console.log(firstEven === 2); // Output: true
+console.log(numbers);
